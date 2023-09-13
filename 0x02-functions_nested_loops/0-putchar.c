105cb38b3fce2b078082603fbdc6d48abd9e94d1
Ayouub-aj <aigle313@gmail.com>
2023-09-12 19:03:46 -0700

    repo stash

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int	main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	ch = 0;
+	while (ch < 8)
+	{
+		_putchar(str[ch]);
+			++ch;
+	}
+	_putchar('\n');
+	return (0);
+}
